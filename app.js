c236f195ebd1f80241510047e93f80e4eb3254e7
veerapspk <veeraveera95422@gmail.com>
2023-04-26 14:42:19 +0000

    jkjfkd

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 const dbPath = path.join(__dirname, "twitterClone.db");
 
 let db = null;
