216dd5be14ed286b9822cc4895705ec38d45c645
veerapspk <veeraveera95422@gmail.com>
2023-04-27 11:10:21 +0000

    fdfd

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
